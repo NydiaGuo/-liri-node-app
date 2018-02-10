e825ce661b3d65406bca722d51b6e84aa8d6ae4d
NI GUO <guoni0909@gmail.com>
2018-02-09 16:49:08 -0800

    added ignore file and updated the keys file

---
+++ keys.js
@@ -0,0 +1,29 @@
+console.log('this is loaded');
+var omdKey = "4f09a22c";
+
+//OMDb API: http://www.omdbapi.com/?i=tt3896198&apikey=4f09a22c
+
+var twitterKeys = {
+  consumer_key: '<input here>',
+  consumer_secret: '<input here>',
+  access_token_key: '<input here>',
+  access_token_secret: '<input here>',
+}
+
+module.exports = twitterKeys;
+
+// var Twitter = require('twitter');
+ 
+// var client = new Twitter({
+//   consumer_key: '',
+//   consumer_secret: '',
+//   access_token_key: '',
+//   access_token_secret: ''
+// });
+ 
+// var params = {screen_name: 'nodejs'};
+// client.get('statuses/user_timeline', params, function(error, tweets, response) {
+//   if (!error) {
+//     console.log(tweets);
+//   }
+// });
